7ab3d9ead177c73d40363240309ff68160d36cb2
jsu27 <jocelin.su@gmail.com>
2023-07-05 14:24:09 -0700

    update

--- slideshow.js
+++ slideshow.js
@@ -20,29 +20,11 @@ function showSlides(n) {
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-    // curVideo = slides[i].getElementsByTagName('video')[0];
-    // curVideo.currentTime = 0;
-    // curVideo.pause();
-    // slides[i].getElementsByTagName('video')[0].currentTime = 0; // reset all videos
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  // console.log(slideIndex);
-  // console.log(slides);
-  // console.log(slides[0]);
-  // console.log(slides[slideIndex-1]);
-  // slides[slideIndex-1].getElementsByTagName('video')[0].play(); // replay video on click
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
-
-// var media=document.querySelector('video');
-
-// btn.addEventListener("click",function(){
-//   media.currentTime = 0;
-// })
-// play.addEventListener("click", function(){
-//   media.play();
-// })
